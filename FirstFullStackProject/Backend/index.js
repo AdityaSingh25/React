05ee5e53fb44499655a95475a739793d776bc353
AdityaSingh25 <richiaditya25@gmail.com>
2024-12-31 18:03:26 +0530

    zod validation created

--- FirstFullStackProject/Backend/index.js
+++ FirstFullStackProject/Backend/index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
+
 const PORT = 3000;
+
+
+app.post("/todo", function (req, res) {});
+
+app.get("/todos", function (req, res) {});
+
+app.put("/completed", function (req, res) {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
